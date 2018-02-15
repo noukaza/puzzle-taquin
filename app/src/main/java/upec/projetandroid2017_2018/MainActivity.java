3a3ebf3eda1289b5bf93d19d40c9b6ffc13f4efd
Noureddine Amrane <noureddine.amrane@etu.u-pec.fr>
2018-02-15 16:36:26 +0100

    add permission for vibrate to use it for clic button

--- app/src/main/java/upec/projetandroid2017_2018/MainActivity.java
+++ app/src/main/java/upec/projetandroid2017_2018/MainActivity.java
@@ -1,7 +1,10 @@
 package upec.projetandroid2017_2018;
 
+import android.content.Intent;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -10,8 +13,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
+
+    public void letsGo(View view) {
+        Vibrator vib = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        vib.vibrate(50);
+    }
+
 }
