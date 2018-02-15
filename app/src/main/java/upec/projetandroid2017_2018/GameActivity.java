d885bba21b508a118efb434f37398690b80cccf0
Noureddine Amrane <noureddine.amrane@etu.u-pec.fr>
2018-02-15 17:08:08 +0100

    add game activity and add method let's go for the button

---
+++ app/src/main/java/upec/projetandroid2017_2018/GameActivity.java
@@ -0,0 +1,15 @@
+package upec.projetandroid2017_2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/upec/projetandroid2017_2018/MainActivity.java
+++ app/src/main/java/upec/projetandroid2017_2018/MainActivity.java
@@ -9,18 +9,25 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    private   Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+      //  requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
     }
 
     public void letsGo(View view) {
         Vibrator vib = (Vibrator) getSystemService(VIBRATOR_SERVICE);
         vib.vibrate(50);
+
+        intent = new Intent(MainActivity.this,GameActivity.class);
+        startActivity(intent);
+
+
     }
+
 
 }
