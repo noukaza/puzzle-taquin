a971b1d81eece5d56a1280e95b601c84abbeb3e6
noukaza <noukaza@gmail.com>
2018-02-24 12:32:29 +0100

    design mainActivty

--- app/src/main/java/upec/projetandroid2017_2018/MainActivity.java
+++ app/src/main/java/upec/projetandroid2017_2018/MainActivity.java
@@ -5,19 +5,24 @@ import android.media.MediaPlayer;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageView;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     private   Intent intent;
     public static boolean VIBRATION = true , SOND = true;
+    ImageButton vibrationButton , soundButton ,helpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
+        vibrationButton = (ImageButton) findViewById(R.id.vibButton);
+        soundButton = (ImageButton) findViewById(R.id.soundButton);
+        helpButton = (ImageButton) findViewById(R.id.helpButton);
     }
 
     public void letsGo(View view) {
@@ -30,17 +35,17 @@ public class MainActivity extends AppCompatActivity {
     public void changeVibration(View view){
         VIBRATION = !VIBRATION;
         if(VIBRATION)
-            view.animate().rotation(0);
+            vibrationButton.setImageResource(R.mipmap.ic_vibration_white_24dp);
         else
-            view.animate().rotation(90);
+            vibrationButton.setImageResource(R.mipmap.ic_phonelink_erase_white_24dp );
         VibrationAndClicSound();
     }
     public void changeSond(View view){
         SOND = !SOND;
         if(SOND)
-            view.animate().rotation(0);
+            soundButton.setImageResource(R.mipmap.ic_queue_music_white_24dp);
         else
-            view.animate().rotation(90);
+            soundButton.setImageResource(R.mipmap.ic_volume_mute_white_24dp);
         VibrationAndClicSound();
     }
     public void help(View view){
